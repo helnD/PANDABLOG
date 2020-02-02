ed1af610c86e0d8eb4544dd831d3c589075d2868
Vladimir <vladimirrudt@yandex.ru>
2020-02-02 18:16:36 +0700

    It's a broken version. I can't bind my logic to the design.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using WebApplication.Models;
 
 namespace WebApplication
@@ -37,23 +37,33 @@ namespace WebApplication
             services.AddScoped(provider => new BlogBuilder()
                 .Repository(new SqlRepository(context))
                 .Build());
+
             
-            services.AddControllersWithViews();
+            
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
 
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware();
+            }
+            
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(endpoints =>
             {
-                endpoints.MapControllerRoute(
+                endpoints.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "api/{controller=Home}/{action=Index}");
+                endpoints.MapSpaFallbackRoute("spa-fallback", new 
+                    { controller = "Home", action = "Index" });
             });
         }
     }
